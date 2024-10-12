f7bf45d6878512891f3b88ed92e7afd680810f32
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-10-12 14:21:56 -0300

    arrumando pastas

--- ws/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb+srv://salaoUser:brIBzx32MiodJOD3@cluster0.lky72.mongodb.net/salaoUser?retryWrites=true&w=majority&appName=Cluster0';
-
-mongoose
-    .connect(URI)
-    .then(() => console.log('Db is Up!'))
-    .catch((err) => {
-        console.error('Erro ao conectar ao banco de dados:', err.message); // Log detalhado do erro
-    });
